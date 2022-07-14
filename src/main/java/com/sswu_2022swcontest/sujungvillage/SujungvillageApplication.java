484faa62ab258ef4bf4ce391cf861176e8f8ade7
yooputer <yooputer@gmail.com>
2022-07-14 14:13:14 +0900

    [ADD] initial files

---
+++ src/main/java/com/sswu_2022swcontest/sujungvillage/SujungvillageApplication.java
@@ -0,0 +1,13 @@
+package com.sswu_2022swcontest.sujungvillage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SujungvillageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SujungvillageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sswu_2022swcontest/sujungvillage/SujungvillageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sswu_2022swcontest.sujungvillage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SujungvillageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
