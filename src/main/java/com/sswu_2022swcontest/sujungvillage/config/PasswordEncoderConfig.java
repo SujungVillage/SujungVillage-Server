04ac9c98189c1de8ed23cb7d69bc5ba8ea35683d
yooputer <yooputer@gmail.com>
2022-07-19 08:48:24 +0900

    [ADD] SpringSecurityConfig, PasswordEncoderConfig 작성

---
+++ src/main/java/com/sswu_2022swcontest/sujungvillage/config/PasswordEncoderConfig.java
@@ -0,0 +1,16 @@
+package com.sswu_2022swcontest.sujungvillage.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class PasswordEncoderConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+}
