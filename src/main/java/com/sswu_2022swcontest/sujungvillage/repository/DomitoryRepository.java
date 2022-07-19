8d57a45e9c82264e1a4ac48ac98b86934695adcf
yooputer <yooputer@gmail.com>
2022-07-19 09:26:32 +0900

    [FEAT] addDomitory메서드 구현

--- src/main/java/com/sswu_2022swcontest/sujungvillage/repository/DomitoryRepository.java
+++ src/main/java/com/sswu_2022swcontest/sujungvillage/repository/DomitoryRepository.java
@@ -5,4 +5,5 @@ import com.sswu_2022swcontest.sujungvillage.entity.Resident;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DomitoryRepository extends JpaRepository<Domitory, Long> {
+    Domitory findByDomitoryName(String domitoryName);
 }
--- src/main/java/com/sswu_2022swcontest/sujungvillage/service/DomitoryService.java
+++ src/main/java/com/sswu_2022swcontest/sujungvillage/service/DomitoryService.java
@@ -1,5 +1,7 @@
 package com.sswu_2022swcontest.sujungvillage.service;
 
+import com.sswu_2022swcontest.sujungvillage.entity.Domitory;
+import com.sswu_2022swcontest.sujungvillage.entity.User;
 import com.sswu_2022swcontest.sujungvillage.repository.DomitoryRepository;
 import com.sswu_2022swcontest.sujungvillage.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -11,4 +13,11 @@ public class DomitoryService {
     private final DomitoryRepository domitoryRepository;
 
     // TODO: addDomitory메서드 구현
+    public Domitory addDomitory(String domitoryName, String address) {
+        if (domitoryRepository.findByDomitoryName(domitoryName) != null){
+            System.out.println("이미 존재하는 기숙사");
+        }
+
+        return domitoryRepository.save(new Domitory(null, domitoryName, address));
+    }
 }
