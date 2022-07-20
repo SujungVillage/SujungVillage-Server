897fe431a43fed176045b86efa9bc37e60b1fea8
yooputer <yooputer@gmail.com>
2022-07-20 14:34:55 +0900

    [FEAT] ResiedntService의 addResident 메서드 구현

--- src/main/java/com/sswu_2022swcontest/sujungvillage/repository/ResidentRepository.java
+++ src/main/java/com/sswu_2022swcontest/sujungvillage/repository/ResidentRepository.java
@@ -1,10 +1,7 @@
 package com.sswu_2022swcontest.sujungvillage.repository;
 
 import com.sswu_2022swcontest.sujungvillage.entity.Resident;
-import com.sswu_2022swcontest.sujungvillage.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-
-public interface ResidentRepository extends JpaRepository<Resident, String> {
-    // 기숙사별 학생들 리스트 반환 메서드
+public interface ResidentRepository extends JpaRepository<Resident, Long> {
 }
--- src/main/java/com/sswu_2022swcontest/sujungvillage/service/ResidentService.java
+++ src/main/java/com/sswu_2022swcontest/sujungvillage/service/ResidentService.java
@@ -1,15 +1,21 @@
 package com.sswu_2022swcontest.sujungvillage.service;
 
+import com.sswu_2022swcontest.sujungvillage.entity.Domitory;
+import com.sswu_2022swcontest.sujungvillage.entity.Resident;
+import com.sswu_2022swcontest.sujungvillage.entity.User;
 import com.sswu_2022swcontest.sujungvillage.repository.ResidentRepository;
-import com.sswu_2022swcontest.sujungvillage.repository.UserRepository;
+import com.sun.istack.NotNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class ResidentService {
-    private final UserRepository userRepository;
     private final ResidentRepository residentRepository;
 
     // TODO: addResident메서드 구현
+    public Resident addResident(@NotNull User user, @NotNull Domitory domitory, @NotNull String detailedAddress) {
+
+        return residentRepository.save(new Resident(null, user, domitory, detailedAddress));
+    }
 }
