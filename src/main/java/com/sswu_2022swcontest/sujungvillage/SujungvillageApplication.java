3c953e85fa6c2dfae1978362e8a88d2d51f91c8b
yooputer <yooputer@gmail.com>
2022-08-11 19:34:37 +0900

    [FIX] SujungVillageApplication에 코드 추가

--- src/main/java/com/sswu_2022swcontest/sujungvillage/SujungvillageApplication.java
+++ src/main/java/com/sswu_2022swcontest/sujungvillage/SujungvillageApplication.java
@@ -2,8 +2,10 @@ package com.sswu_2022swcontest.sujungvillage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SujungvillageApplication {
 
     public static void main(String[] args) {
