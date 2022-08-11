74aadc53837e5be189f25f71da53ad90539b2de3
yooputer <yooputer@gmail.com>
2022-08-11 19:33:49 +0900

    [FEAT] UserService에 현재 인증된 User 객체 반환 메서드 구현

--- src/main/java/com/sswu_2022swcontest/sujungvillage/service/UserService.java
+++ src/main/java/com/sswu_2022swcontest/sujungvillage/service/UserService.java
@@ -4,6 +4,7 @@ import com.sswu_2022swcontest.sujungvillage.entity.User;
 import com.sswu_2022swcontest.sujungvillage.repository.UserRepository;
 import com.sswu_2022swcontest.sujungvillage.repository.home.LmpRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -14,6 +15,18 @@ public class UserService {
 
     private final UserRepository userRepo;
     private final LmpRepository lmpRepo;
+
+    public User getUser(){
+        String userId = SecurityContextHolder.getContext().getAuthentication().getName();
+
+        Optional<User> user = userRepo.findById(userId);
+
+        if (user.isPresent()) {
+            return user.get();
+        }
+
+        return null;
+    }
 
     public Boolean isResident(String userId) {
         Optional<User> user = userRepo.findById(userId);
