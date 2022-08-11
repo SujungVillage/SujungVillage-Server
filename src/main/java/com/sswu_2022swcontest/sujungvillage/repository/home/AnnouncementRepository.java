90e5d3f78c054648b89ceb4506331fcb4d47ed3e
yooputer <yooputer@gmail.com>
2022-08-11 11:09:20 +0900

    [FIX] AnnouncementRepository 코드 수정

--- src/main/java/com/sswu_2022swcontest/sujungvillage/repository/home/AnnouncementRepository.java
+++ src/main/java/com/sswu_2022swcontest/sujungvillage/repository/home/AnnouncementRepository.java
@@ -1,6 +1,7 @@
 package com.sswu_2022swcontest.sujungvillage.repository.home;
 
+import com.sswu_2022swcontest.sujungvillage.entity.home.Announcement;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AnnouncementRepository extends JpaRepository<AnnouncementRepository, Long> {
+public interface AnnouncementRepository extends JpaRepository<Announcement, Long> {
 }
