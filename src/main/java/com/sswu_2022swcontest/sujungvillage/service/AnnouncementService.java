fc03bc35c61679a64b8deb853e8f327bb8f8e10f
yooputer <yooputer@gmail.com>
2022-08-16 09:14:52 +0900

    [FEAT] 새공지사항 등록시 알림기능 추가

--- src/main/java/com/sswu_2022swcontest/sujungvillage/service/AnnouncementService.java
+++ src/main/java/com/sswu_2022swcontest/sujungvillage/service/AnnouncementService.java
@@ -5,6 +5,7 @@ import com.sswu_2022swcontest.sujungvillage.dto.dto.announcement.SimpleAnnouncem
 import com.sswu_2022swcontest.sujungvillage.entity.Dormitory;
 import com.sswu_2022swcontest.sujungvillage.entity.home.Announcement;
 import com.sswu_2022swcontest.sujungvillage.repository.DormitoryRepository;
+import com.sswu_2022swcontest.sujungvillage.repository.UserRepository;
 import com.sswu_2022swcontest.sujungvillage.repository.home.AnnouncementRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -19,12 +20,42 @@ public class AnnouncementService {
     private final AnnouncementRepository annoRepo;
     private final DormitoryRepository dormitoryRepo;
     private final UserService userService;
+    private final FcmService fcmService;
+    private final UserRepository userRepo;
 
     // 공지사항 작성
     public AnnouncementDTO writeAnnouncement(String title, String content, String dormitoryName) {
 
         Dormitory dormitory = dormitoryRepo.findByDormitoryName(dormitoryName)
                 .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 기숙사명, 기숙사명="+dormitoryName));
+
+        // 알람 보내기
+
+        String briefTitle = title;
+
+        if (briefTitle.length() > 20) {
+            briefTitle = title.substring(0, 20)+"...";
+        }
+
+        if (dormitoryName.equals("전체")) {
+            String finalBriefTitle = briefTitle;
+            userRepo.findAll().stream().forEach(user -> {
+                fcmService.sendMessageTo(
+                    fcmService.getDeviceToken(user.getId()),
+                            "새로운 공지사항이 등록되었습니다.",
+                        finalBriefTitle
+                    );
+            });
+        } else {
+            String finalBriefTitle = briefTitle;
+            userRepo.findAllByDormitoryId(dormitory.getId()).stream().forEach(user -> {
+                fcmService.sendMessageTo(
+                        fcmService.getDeviceToken(user.getId()),
+                        "새로운 공지사항이 등록되었습니다.",
+                        finalBriefTitle
+                );
+            });
+        }
 
         return AnnouncementDTO.entityToDTO(
                 annoRepo.save(new Announcement(
