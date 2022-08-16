ddea2fb159db46ff05c2631fe887a6752a0c3494
yooputer <yooputer@gmail.com>
2022-08-16 09:20:12 +0900

    [FIX] UserRepository에 findAllByDormitoryId메서드 추가

--- src/main/java/com/sswu_2022swcontest/sujungvillage/repository/UserRepository.java
+++ src/main/java/com/sswu_2022swcontest/sujungvillage/repository/UserRepository.java
@@ -3,5 +3,9 @@ package com.sswu_2022swcontest.sujungvillage.repository;
 import com.sswu_2022swcontest.sujungvillage.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Arrays;
+import java.util.List;
+
 public interface UserRepository extends JpaRepository<User, String> {
+    List<User> findAllByDormitoryId(Integer id);
 }
