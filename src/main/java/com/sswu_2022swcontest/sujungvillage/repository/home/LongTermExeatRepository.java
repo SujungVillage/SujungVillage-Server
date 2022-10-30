b02cd4bfac88030c1dce9e459d70a510e6be017a
yooputer <yooputer@gmail.com>
2022-10-30 10:33:39 +0900

    [ADD] LongTermExeatRepository 추가

---
+++ src/main/java/com/sswu_2022swcontest/sujungvillage/repository/home/LongTermExeatRepository.java
@@ -0,0 +1,8 @@
+package com.sswu_2022swcontest.sujungvillage.repository.home;
+
+import com.sswu_2022swcontest.sujungvillage.entity.home.LongTermExeat;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LongTermExeatRepository extends JpaRepository<LongTermExeat, Long> {
+    
+}
